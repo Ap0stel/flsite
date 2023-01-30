3dbc23f9113aa925c874d867c0757184f89c0609
Фёдор <rabota_feodor@mail.ru>
2023-01-30 11:21:17 +0300

    base шаблон

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ['Установки', 'Первое приложение' , 'Обратная связь']
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Главная', menu=menu)
+
 
 @app.route("/about")
 def about():
-    return '<h1>About IIIgor<h1>'
+    return render_template('about.html', title='О сайте', menu=menu)
+
 
 @app.route("/about/Pasha")
 def pasha():
     return '<h1>Pasha vlojen<h1>'
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
