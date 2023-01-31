269c5d8d043dc7854535b7d7b5ec49f6a8e39d62
Фёдор <rabota_feodor@mail.ru>
2023-01-31 14:14:35 +0300

    db mainmenu cinnect(не работает меню сайта. не достаются из БД данные)

--- flsite.py
+++ flsite.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template, url_for, request, session, redirect, abort
+from flask import Flask, render_template, url_for, request, session, redirect, abort, g
 import sqlite3
 import os
-DATABASE  = '/tmp/flsite.db'
+from FDataBase import FDataBase
+
+DATABASE = '/tmp/flsite.db'
 DEBUG = True
 SECRET_KEY = 'sdfsdfgdsfgsdg345g'
+
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    '''Вспомогательная функция для слздания БД без запуска сервера'''
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    '''Соединение с БД, если оно не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
 
 menu = [{'name': 'Установка', 'url': 'install-flask'},
         {'name': 'Первое приложение', 'url': 'first-app'},
@@ -14,8 +42,10 @@ menu = [{'name': 'Установка', 'url': 'install-flask'},
 
 @app.route("/")
 def index():
+    db = get_db()
+    dbase = FDataBase(db)
     print(url_for('index'))
-    return render_template('index.html', title='Главная', menu=menu)
+    return render_template('index.html', title='Главная', menu=dbase.getMenu())
 
 
 @app.route("/about")
@@ -33,7 +63,8 @@ def pasha():
 
 @app.errorhandler(404)
 def pageNotFound(error):
-    return render_template('page404.html', title='Страница не найдена',  menu=menu)
+    return render_template('page404.html', title='Страница не найдена', menu=menu)
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -45,6 +76,7 @@ def login():
 
     return render_template('login.html', title='Авторизация', menu=menu)
 
+
 @app.route('/profile/<username>')
 def profile(username):
     if 'userLogged' not in session or session['userLogged'] != username:
@@ -52,6 +84,11 @@ def profile(username):
     return f'Профиль пользователя: {username}'
 
 
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрытие соединения с БД, если оно установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 if __name__ == "__main__":
